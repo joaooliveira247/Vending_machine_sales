f3af9ba418fee01ef769d12fe98196561772365d
joaooliveira247 <joaooliveira247@gmail.com>
2023-07-18 15:09:54 -0300

    :sparkles: add class to settings

--- vending_machine_sales/core/config.py
+++ vending_machine_sales/core/config.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 class Settings:
     BASE_DIR = Path(__file__).parent.parent
-    BASE_CSV_DIR = (BASE_DIR / "csv").__str__()
+    BASE_CSV_DIR = BASE_DIR / "csv"
 
 
 settings = Settings()
