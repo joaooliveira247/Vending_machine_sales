157175bb391e0801d2afd89fa97fe0328d7694ff
joaooliveira247 <joaooliveira247@gmail.com>
2023-07-21 18:54:59 -0300

    :sparkles: add function to replace null fields

--- vending_machine_sales/pipeline/functions.py
+++ vending_machine_sales/pipeline/functions.py
@@ -1,4 +1,5 @@
 from pandas import DataFrame, Series
+from typing import Any
 
 
 def null_fields(df: DataFrame) -> list[tuple[str, int]]:
@@ -10,3 +11,12 @@ def null_fields(df: DataFrame) -> list[tuple[str, int]]:
     null_fields = list(zip(null_fields_count.index, null_fields_count))
 
     return [null_field for null_field in null_fields if null_field[1] > 0]
+
+
+def replace_null_values(df: DataFrame, replace: dict[str, Any]) -> DataFrame:
+    for k, v in replace.items():
+        try:
+            df[k] = df[k].fillna(v)
+        except Exception:
+            raise KeyError(f"field {k} not found.")
+    return df
