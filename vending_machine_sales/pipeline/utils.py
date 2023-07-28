e05db6cebfe0f82453b9c03f3e71e0c277ab783a
joaooliveira247 <joaooliveira247@gmail.com>
2023-07-28 18:54:02 -0300

    :sparkles: add arg to match type of format

--- vending_machine_sales/pipeline/utils.py
+++ vending_machine_sales/pipeline/utils.py
@@ -1,2 +1,6 @@
-def percent_format(x: float, total: int | float) -> str:
-    return "{:.2f} %\n{}".format(x, round(total * (x / 100)))
+def numeric_format(type_: str, x: float, total: int | float) -> str:
+    match type_:
+        case "percentage":
+            return "{:.2f} %\n{}".format(x, round(total * (x / 100)))
+        case "money":
+            return "{:.2f} %\nUSD: {:.2f}".format(x, total * (x / 100))
