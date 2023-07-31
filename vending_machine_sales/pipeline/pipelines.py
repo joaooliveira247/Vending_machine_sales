e4f52bf2f0586a7851ae071f7742f0cd5a42c4e2
joaooliveira247 <joaooliveira247@gmail.com>
2023-07-31 20:33:38 -0300

    :hammer: refactor classes and create base interface

--- vending_machine_sales/pipeline/pipelines.py
+++ vending_machine_sales/pipeline/pipelines.py
@@ -4,14 +4,15 @@ from vending_machine_sales.pipeline.functions import (
 )
 from vending_machine_sales.pipeline.utils import numeric_format
 from pandas import DataFrame
+from abc import ABC, abstractmethod
 
 
-class MostSellPipeline:
+class BasePipeline(ABC):
     def __init__(self, df: DataFrame) -> None:
         self.df = df
 
     @staticmethod
-    def __plot(
+    def _plot(
         df: DataFrame,
         n_plot: int,
         field: str,
@@ -27,7 +28,7 @@ class MostSellPipeline:
         return
 
     @staticmethod
-    def __group_by(
+    def _group_by(
         df: DataFrame, field: str, agg: str, rename: str = None
     ) -> DataFrame:
         group = (
@@ -40,53 +41,38 @@ class MostSellPipeline:
         group = group.sort_values(rename, ascending=False)
         return group
 
+    @abstractmethod
+    def by_amount() -> DataFrame | None:
+        ...
+
+    @abstractmethod
+    def by_income() -> DataFrame | None:
+        ...
+
+
+class MostSellPipeline(BasePipeline):
     def by_amount(self, n_plot: int = None) -> DataFrame | None:
-        most_sell_amount = self.__group_by(self.df, "MQty", "count", "Amount")
+        most_sell_amount = super()._group_by(
+            self.df, "MQty", "count", "Amount"
+        )
 
         if n_plot:
-            self.__plot(most_sell_amount, n_plot, "Amount", "percentage")
+            super()._plot(most_sell_amount, n_plot, "Amount", "percentage")
             return
 
         return most_sell_amount
 
     def by_income(self, n_plot: int = None) -> DataFrame | None:
-        most_sell_income = self.__group_by(self.df, "MPrice", "sum", "Income")
+        most_sell_income = super()._group_by(
+            self.df, "MPrice", "sum", "Income"
+        )
 
         if n_plot:
-            self.__plot(most_sell_income, n_plot, "Income", "money")
+            super()._plot(most_sell_income, n_plot, "Income", "money")
             return
 
         return most_sell_income
 
 
-class BestPlacePipeline:
-    def __init__(self, df: DataFrame) -> None:
-        self.df = df
-
-    @staticmethod
-    def __plot(
-        df: DataFrame,
-        n_plot: int,
-        field: str,
-        type_: str,
-    ) -> None:
-        df = df.head(n_plot)
-        df.plot.pie(
-            y=field,
-            figsize=(18, 18),
-            autopct=lambda x: numeric_format(type_, x, df[field].sum()),
-            title="Most Sell Products",
-        )
-        return
-
-    @staticmethod
-    def __group_by(
-        df: DataFrame, field: str, agg: str, rename: str = None
-    ) -> DataFrame:
-        group = (
-            df[["Product", field]]
-            .groupby(by=["Product"])
-            .agg({field: agg})
-            .rename(columns={field: rename})
-        )
-
+class BestPlacePipeline(BasePipeline):
+    ...
