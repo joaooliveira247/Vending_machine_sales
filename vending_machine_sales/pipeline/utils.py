5585a543a8666323c3f64b43151c78ff8ef599a4
joaooliveira247 <joaooliveira247@gmail.com>
2023-08-01 18:15:24 -0300

    :hammer: change str return

--- vending_machine_sales/pipeline/utils.py
+++ vending_machine_sales/pipeline/utils.py
@@ -1,6 +1,6 @@
 def numeric_format(type_: str, x: float, total: int | float) -> str:
     match type_:
         case "percentage":
-            return "{:.2f} %\n{}".format(x, round(total * (x / 100)))
+            return "{:.2f} %\n{} units".format(x, round(total * (x / 100)))
         case "money":
             return "{:.2f} %\nUSD: {:.2f}".format(x, total * (x / 100))
