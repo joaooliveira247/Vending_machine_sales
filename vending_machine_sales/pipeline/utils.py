150e2dbe4dea74b40b79266f0c8359cdfbe82239
joaooliveira247 <joaooliveira247@gmail.com>
2023-08-04 21:54:10 -0300

    :sparkles: add enum to type_ option in format function

--- vending_machine_sales/pipeline/utils.py
+++ vending_machine_sales/pipeline/utils.py
@@ -1,6 +1,12 @@
-def numeric_format(type_: str, x: float, total: int | float) -> str:
+from enum import Enum
+
+class TypeFormat(Enum):
+    monetary = None,
+    amount = None
+
+def numeric_format(type_: TypeFormat, x: float, total: int | float) -> str:
     match type_:
-        case "percentage":
+        case TypeFormat.amount:
             return "{:.2f} %\n{} units".format(x, round(total * (x / 100)))
-        case "money":
+        case TypeFormat.monetary:
             return "{:.2f} %\nUSD: {:.2f}".format(x, total * (x / 100))
